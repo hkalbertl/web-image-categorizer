2235ed7761148071ab75cf1b144e137e92cc42ed
Albert L <hkalbertl@users.noreply.github.com>
2025-02-21 02:23:05 +0800

    Used case-insensitive matching for folder name

--- webpack.config.ts
+++ webpack.config.ts
@@ -51,12 +51,8 @@ const config: webpack.Configuration = {
           info: { minimized: true }
         },
         {
-          from: "node_modules/@fortawesome/fontawesome-free/css/all.min.css",
-          to: "lib/fontawesome/css/all.min.css"
-        },
-        {
-          from: "node_modules/@fortawesome/fontawesome-free/webfonts",
-          to: "lib/fontawesome/webfonts"
+          from: "node_modules/bootstrap-icons/font",
+          to: "lib/bootstrap-icons"
         }
       ],
     })
@@ -67,77 +63,3 @@ const config: webpack.Configuration = {
   }
 };
 export default config;
-
-
-/*
-export default (env: { production?: boolean }): webpack.Configuration => {
-  const isProduction = env.production;
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-
-  const config: webpack.Configuration = {
-    mode: isProduction ? "production" : "development",
-    entry: {
-      background: "./src/js/background.ts",
-      popup: "./src/js/popup.ts",
-      sidebar: "./src/js/sidebar.ts",
-      options: "./src/js/options.ts"
-    },
-    output: {
-      filename: "js/[name].js",
-      path: path.resolve(__dirname, "dist")
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          use: 'ts-loader',
-          exclude: /node_modules/
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['.ts', '.js']
-    },
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: "src",
-            to: ".",
-            globOptions: {
-              ignore: ["** /*.ts"], // Ignore all .ts files
-            },
-          },
-          {
-            from: "node_modules/bootstrap/dist/css/bootstrap.min.css",
-            to: "lib/bootstrap/css/bootstrap.min.css",
-            info: { minimized: true }
-          },
-          {
-            from: "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
-            to: "lib/bootstrap/js/bootstrap.bundle.min.js",
-            info: { minimized: true }
-          },
-          {
-            from: "node_modules/@fortawesome/fontawesome-free/css/all.min.css",
-            to: "lib/fontawesome/css/all.min.css"
-          },
-          {
-            from: "node_modules/@fortawesome/fontawesome-free/webfonts",
-            to: "lib/fontawesome/webfonts"
-          }
-        ],
-      })
-    ],
-    devtool: isProduction ? "source-map" : "inline-source-map",
-    performance: {
-      hints: false, // Disable asset size warnings
-    }
-  };
-
-  console.debug(`## Webpack env: ${config.mode}`);
-
-  return config;
-};
-*/
